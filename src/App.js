19af9fc33c109d06d5fc707b24d182cf7efcb421
jr7072 <rosales.juan7072@gmail.com>
2021-10-02 17:00:02 -0500

    files bruh

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import './Components/Landing/LandingCom'
+import { LandingComponent } from './Components/Landing/LandingCom';
 
 function App() {
   return (
     <div className="App">
-     
+      
+      <section>
+        <LandingComponent />
+      </section>
     </div>
   );
 }
---
+++ src/Pages/Landing/Landing.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export const Landing = () => {
+
+    return(
+
+        <section className="landing">
+        
+            <div className="picture">
+                
+            </div>
+            <div className="login-section">
+            
+            </div>
+
+        </section>
+    )
+}
