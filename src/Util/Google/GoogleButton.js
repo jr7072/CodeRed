9964fc690061dc206333dd4cdff74d3cb6f3cebe
jr7072 <rosales.juan7072@gmail.com>
2021-10-03 01:16:48 -0500

    google login redirect

--- src/Util/Google/GoogleButton.js
+++ src/Util/Google/GoogleButton.js
@@ -7,15 +7,20 @@ const clientId = "139033022767-4c6ha4fmr7h2nsqhdp5u5sfkdtlajvrc.apps.googleuserc
 
 export const GoogleButton = () => {
 
+    const onAccept = (response) => {
+
+        window.location.href = "http://localhost:3000/home"
+    }
+
     return(
         <GoogleLogin 
             clientId={clientId}
             buttonText="Login"
-            onSuccess={null}
+            onSuccess={onAccept}
             onFailure={null}
             cookiePolicy={'single_host_origin'}
             className="google-button"
-        
+            isSignedIn={true}
         />
     )
 }
