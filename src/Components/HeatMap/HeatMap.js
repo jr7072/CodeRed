5c1c5178fedbfe2bddabc3f4bf4cc013e9410754
Eric Nguyen <47893402+EricNguyen1124@users.noreply.github.com>
2021-10-03 04:00:30 -0500

    beginning heatmap page

---
+++ src/Components/HeatMap/HeatMap.js
@@ -0,0 +1,12 @@
+import {React, useEffect, useState} from 'react'
+import PropTypes from 'prop-types';
+import ReactHeatmap from 'react-heatmap/src/ReactHeatmap';
+export const HeatMap = (props)=> {
+
+    return(
+    <div>
+            <img src="https://rocketlister.com/wp-content/uploads/2019/11/17820-Claremont-2D-Floorplan.jpg"/>
+    </div>
+
+    )
+}
--- src/Pages/IncidentReport/IncidentReportPage.js
+++ src/Pages/IncidentReport/IncidentReportPage.js
@@ -1,7 +1,6 @@
 import React , {useState, useEffect} from 'react';
 import { Table, TableCell } from 'react-bootstrap';
 import axios from 'axios';
-import { GoogleButton } from '../../Util/Google/GoogleButton';
 
 export const IncidentReportPage = () => {
 
@@ -12,11 +11,41 @@ export const IncidentReportPage = () => {
         .get("http://localhost:5000/incident/")
         .then((res) => {
             setIncidents(res.data)
+            //console.log(incidents);
         })
         .catch(function (error) {
           console.log(error);
         });
     });
+
+    const convertIncidentType = (number) => {
+        switch (number){
+            case "1":
+                return "Burns"
+                break;
+            case "2":
+                return "Back Injury"
+                break;
+            case "3":
+                return "Chemical Exposure"
+                break;
+            case "4":
+                return "Falls"
+                break;
+            case "5":
+                return "Combustion"
+                break;
+            case "6":
+                return "Crushing"
+                break;
+            case "7":
+                return "Vehicle Strike"
+                break;
+            case "8":
+                return "Near Miss"
+                break;
+        }
+    }
 
     return(
         <div>
@@ -26,26 +55,24 @@ export const IncidentReportPage = () => {
             <Table className="incidentTable" striped bordered hover>
                 <thead>
                     <tr>
-                    <th>#</th>
-                    <th>First Name</th>
-                    <th>Last Name</th>
-                    <th>Username</th>
+                    <th>Incident Name</th>
+                    <th>Employee</th>
+                    <th>Workplace</th>
+                    <th>Date</th>
+                    <th>Time</th>
+                    <th>Incident Type</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
-                    </tr>
                     {incidents.map((x) => {
                         return(
                             <tr>
                             <td>{x.IncidentName}</td>
                             <td>{x.Employee}</td>
-                            <td>{x.LocationX}</td>
-                            <td>{x.LocationY}</td>
+                            <td>{x.Workplace}</td>
+                            <td>{x.Date}</td>
+                            <td>{x.Time}</td>
+                            <td>{convertIncidentType(x.IncidentType)}</td>
                             </tr>
                         )
                     })}
