060756f2b717243d0ad3e31ec7aaf4a3f206dfa9
Eric Nguyen <Ericnguyen1124@gmail.com>
2021-10-02 23:44:02 -0500

    implement IncidentModel

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -6,14 +6,50 @@ import './Nav.css'
 import {useState} from 'react'
 
 export const NavigationBar = () => {
+
+    const [incidentModel, setIncidentModel] = useState(
+        {
+            IncidentName: "",
+            Date: 0,
+            Time: 0,
+            DateTime: 0,
+            Employee: "",
+            Workplace: "",
+            IncidentType: "",
+            LocationX: 0,
+            LocationY: 0,
+        }
+    );
     const [showReportModal, setShowReportModal] = useState(false);
     const [showPinToFloorModal, setShowPinToFloorModal] = useState(false); 
 
     const handleClose = () => {
+        setIncidentModel ({
+            IncidentName: "",
+            Date: 0,
+            Time: 0,
+            DateTime: 0,
+            Employee: "",
+            Workplace: "",
+            IncidentType: "",
+            LocationX: 0,
+            LocationY: 0,
+        })
         setShowReportModal(false);
     }
 
     const handlePinClose = () => {
+        setIncidentModel ({
+            IncidentName: "",
+            Date: 0,
+            Time: 0,
+            DateTime: 0,
+            Employee: "",
+            Workplace: "",
+            IncidentType: "",
+            LocationX: 0,
+            LocationY: 0,
+        })
         setShowPinToFloorModal(false);
     }
 
@@ -30,6 +66,61 @@ export const NavigationBar = () => {
         setShowPinToFloorModal(false);
         console.log("submitted!")
     }
+
+    const handleOnChange = (formName, formValue) => {
+        if(formName == "formIncidentName")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                IncidentName: formValue
+            }))
+        }
+        else if(formName == "formDate")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                Date: formValue
+            }))
+        }
+        else if(formName == "formTime")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                Time: formValue
+            }))
+        }
+        else if(formName == "formWorkplace")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                Workplace: formValue
+            }))
+        }
+        else if(formName == "formEmployeeName")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                Employee: formValue
+            }))
+        }
+        else if(formName == "formIncidentType")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                IncidentType: formValue
+            }))
+        }
+        else if(formName == "Location")
+        {
+            setIncidentModel(prevState => ({
+                ...prevState,
+                LocationX: formValue.left,
+                LocationY: formValue.top
+            }))
+        }
+        console.log(incidentModel);
+    }
+
     return(
         <div>
             <Navbar bg="light" expand="lg">
@@ -53,10 +144,8 @@ export const NavigationBar = () => {
                     </Navbar.Collapse>
                 </Container>
             </Navbar>
-            <ReportIncidentModal show = {showReportModal} onHide = {handleClose} onNext = {handleNext}/>
-            <PinToFloorMapModal show = {showPinToFloorModal} onHide = {handlePinClose} onSubmit = {handleSubmit}/>
+            <ReportIncidentModal show = {showReportModal} onHide = {handleClose} onNext = {handleNext} onChange = {handleOnChange}/>
+            <PinToFloorMapModal show = {showPinToFloorModal} onHide = {handlePinClose} onSubmit = {handleSubmit} onChange = {handleOnChange}/>
         </div>
-
-        
     )
 }
